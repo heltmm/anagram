b07351b2a13347f344b12cb40c0bd746a45dc0ef
Mark Helt <heltmm@gmail.com>
2017-09-08 11:32:54 -0700

    come up with specific tests

---
+++ spec/anagram_spec.rb
@@ -0,0 +1,26 @@
+require('rspec')
+require('anagram')
+require('pry')
+
+describe('Str#anagram') do
+  newStr = Str.new()
+
+  it('checks if two words are anagrams')do
+    expect(newStr('ruby', 'bury')).to eq(true)
+  end
+  it('checks if two words with different cases are anagrams')do
+    expect(newStr('Tea', 'Eat')).to eq(true)
+  end
+  it('checks if two words are anagrams, if so checks if palindrone')do
+    expect(newStr('racecar', 'racecar')).to eq(true)
+  end
+  it('checks if inputs are words')do
+    expect(newStr('ruby', 'br')).to eq('You need to input actual words!')
+  end
+  it('if not anagagram check if antigram')do
+    expect(newStr('hi', 'bye')).to eq('these words are antigrams')
+  end
+  it('checks if two sentences are anagrams')do
+    expect(newStr('ruby', 'bury')).to eq(true)
+  end
+end
